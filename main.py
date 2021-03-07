3698f3f3616e70c5092e09bc41426be064e4e8b0
Siddhant <siddhant.sandal95@gmail.com>
2021-03-07 23:49:35 +0530

    commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+driver = webdriver.Chrome("./chromedriver")
 driver.get("https://store.hp.com/us/en/pdp/hp-laptop-15t-dy200-2j130av-1/")
 time.sleep(10)
 privacy_accept_ele = driver.find_element_by_xpath('//*[@id="onetrust-accept-btn-handler"]').click()
